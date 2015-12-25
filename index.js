c632113c066440bdb2664e581ad503fcddf55355
Mike Chau <mike@chau.io>
2015-12-24 23:18:57 -0600

    Allow passing in customStatsKey and let CustomStats patch compilation.toJson()

--- index.js
+++ index.js
@@ -12,29 +12,40 @@ var CustomStats = require('webpack-custom-stats-patch');
 
 var DEFAULT_PARAMS = {
   algorithm: 'sha384',
-  regex: (/\.(js|css)$/i)
+  regex: (/\.(js|css)$/i),
+  customStatsKey: 'sris',
+  assetKey: 'integrity'
 };
 
 function SriWebpackPlugin(options) {
   var params = options || {};
   this.algorithm = params.algorithm || DEFAULT_PARAMS.algorithm;
   this.regex = params.regex || DEFAULT_PARAMS.regex;
+  this.customStatsKey = params.customStatsKey || DEFAULT_PARAMS.customStatsKey;
+  this.assetKey = params.assetKey || DEFAULT_PARAMS.assetKey;
 };
 
 SriWebpackPlugin.prototype.apply = function(compiler) {
-  var allowedExtensions = this.regex;
   var sriAlgorithm = this.algorithm;
+  var allowedExtensions = this.regex;
+  var customStatsKey = this.customStatsKey;
+  var assetKey = this.assetKey;
   var sris = {};
 
   compiler.plugin('this-compilation', function(compilation) {
     compilation.plugin('optimize-assets', function(assets, callback) {
       Object.keys(assets).forEach(function(file) {
         var asset = assets[file];
+        var assetStat = {};
         var content;
+        var integrity;
 
         if (file.match(allowedExtensions)) {
           content = asset.source();
-          sris[file] = getSriHash(sriAlgorithm, content);
+          integrity = getSriHash(sriAlgorithm, content);
+          assetStat[assetKey] = integrity;
+          sris[file] = assetStat;
+          compilation.assets[file].integrity = integrity;
         }
       });
 
@@ -45,11 +56,7 @@ SriWebpackPlugin.prototype.apply = function(compiler) {
   compiler.plugin('after-emit', function(compilation, callback) {
     var stats = new CustomStats(compilation);
 
-    stats.addCustomStat('sris', sris);
-
-    compilation.getStats = function() {
-      return stats;
-    }
+    stats.addCustomStat(customStatsKey, sris);
 
     callback();
   });
