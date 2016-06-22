fbae4bd3c41b26dee43628f6a7e6b63a0584b0dd
Mike Chau <mike@chau.io>
2016-06-21 23:22:39 -0500

    Add ci specific test script

---
+++ example/build/assets/594ba52eeaee990e38f7/main-594ba52eeaee990e38f7.js
@@ -0,0 +1,1 @@
+!function(e){function o(t){if(r[t])return r[t].exports;var n=r[t]={exports:{},id:t,loaded:!1};return e[t].call(n.exports,n,n.exports,o),n.loaded=!0,n.exports}var r={};return o.m=e,o.c=r,o.p="/assets/594ba52eeaee990e38f7/",o(0)}([function(e,o,r){e.exports=r(2)},function(e,o){},function(e,o,r){r(1),console.log("hello world!")}]);
